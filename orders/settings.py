1af19021fc4f925c46a3fa5a14fc18f2ff782d7f
Dmitriy <dmitri-rt@ya.ru>
2025-02-12 21:28:12 +0300

    Step_a

--- orders/settings.py
+++ orders/settings.py
@@ -150,9 +150,9 @@ REST_FRAMEWORK = {
 # ...
 
 # Celery settings
-CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://localhost:6379/0"
 
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
